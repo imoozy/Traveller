1b369a1464c2be71e654627662e01094629904ae
imoozy <lucal7421@gmail.com>
2025-12-07 23:55:35 -0300

    fix commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+        console.log("Página carregada com sucesso!");
+        alert("Bem-vindo ao Traveller!");
+
+        //nota média
+
+        //var n1 = Number(prompt("Digite sua nota: "));
+        //var n2 = Number(prompt("Digite sua segunda nota: "));
+        //var media = (n1 + n2) / 2;
+
+        //alert("Sua média é: " + media);
+
+        //salário liquido   
+        //var hoasTrab = Number(prompt("Digite o número de horas trabalhadas no mês: "));
+        //var valorHora = Number(prompt("Digite o valor da sua hora de trabalho: "));
+        //var imposto = Number(prompt("Digite o percentual de imposto (em %): "));
+
+        //var salarioBruto = hoasTrab * valorHora;
+        //var desconto = (imposto / 100) * salarioBruto;
+        //var salarioLiquido = salarioBruto - desconto;
+        //alert("Seu salário líquido é: R$ " + salarioLiquido.toFixed(2));
+
+        //consumo de combustível
+        //var distancia = Number(prompt("Digite a distância percorrida em km: "));
+        //var litrosConsumidos = Number(prompt("Digite a quantidade de litros consumidos: "));
+        //var consumoMedio = distancia / litrosConsumidos;
+        //alert("O consumo médio do veículo é: " + consumoMedio.toFixed(2) + " km/l");
+
+        //conversão de temperatura
+        //var celsius = Number(prompt("Digite a temperatura em Celsius: "));
+        //var fahrenheit = (celsius * 9/5) + 32;
+        //alert(celsius + "°C equivalem a " + fahrenheit.toFixed(2) + "°F");
+
+        //área do círculo
+        //var raio = Number(prompt("Digite o raio do círculo: "));
+        //var area = Math.PI * Math.pow(raio, 2);
+        //alert("A área do círculo é: " + area.toFixed(2));
+
+        //replace nome
+        //var mensagem = "Muito obrigado, **NOME**, por participar!";
+        //var nome = prompt("Digite seu nome:");
+        //var newName = mensagem.replace("**NOME**", nome);
+        //alert(newName);
+        
